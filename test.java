1d494cb772397f0fdae070c617d4051d08681a48
shivakumar <shivakumar.drive1@gmail.com>
2023-12-08 17:13:09 +0530

    gffghj

--- test.java
+++ test.java
@@ -1,51 +1,74 @@
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import org.w3c.dom.*;
 import org.xml.sax.InputSource;
 
-import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpression;
-import javax.xml.xpath.XPathFactory;
+import javax.xml.parsers.DocumentBuilder;
 import java.io.StringReader;
 
 public class test {
+    public static void main(String[] args) {
+        String xmlString = "<root><title>Main Title</title><content>Some content<subsection>Subsection</subsection>More content</content></root>";
 
-    public static void main(String[] args) throws Exception {
-        // Your XML document as a string
-        String xmlContent = "<root><person><name>John</name><age>30</age></person><person><name>Jane</name><age>25</age></person></root>";
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            InputSource inputSource = new InputSource(new StringReader(xmlString));
+            Document document = builder.parse(inputSource);
 
-        // Parse the XML content into a Document
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = factory.newDocumentBuilder();
-        InputSource inputSource = new InputSource(new StringReader(xmlContent));
-        Document document = builder.parse(inputSource);
+            // Extract text content
+            String title = extractTitle(document);
+            String content = extractContent(document);
 
-        // Split the document into sub-documents (assuming each person is a sub-document)
-        XPathFactory xPathFactory = XPathFactory.newInstance();
-        XPath xPath = xPathFactory.newXPath();
-        XPathExpression expression = xPath.compile("/root/person");
-        NodeList personNodes = (NodeList) expression.evaluate(document, XPathConstants.NODESET);
-
-        // Process each sub-document
-        for (int i = 0; i < personNodes.getLength(); i++) {
-            Node personNode = personNodes.item(i);
-
-            // Extract fields using XPath expressions within the sub-document
-            String name = extractField(personNode, "name/text()");
-            String age = extractField(personNode, "age/text");
-
-            // Do something with the extracted fields
-            System.out.println("Person " + (i + 1) + ": Name=" + name + ", Age=" + age);
+            // Print the results
+            System.out.println("Title: " + title);
+            System.out.println("Content: " + content);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
-    private static String extractField(Node contextNode, String xpathExpression) throws Exception {
-        XPathFactory xPathFactory = XPathFactory.newInstance();
-        XPath xPath = xPathFactory.newXPath();
-        XPathExpression expression = xPath.compile(xpathExpression);
-        return (String) expression.evaluate(contextNode, XPathConstants.STRING);
+    private static String extractTitle(Document document) {
+        NodeList titleNodes = document.getElementsByTagName("title");
+        if (titleNodes.getLength() > 0) {
+            return titleNodes.item(0).getTextContent();
+        }
+        return null;
+    }
+
+    private static String extractContent(Document document) {
+        StringBuilder contentBuilder = new StringBuilder();
+        NodeList nodes = document.getDocumentElement().getChildNodes();
+
+        for (int i = 0; i < nodes.getLength(); i++) {
+            Node node = nodes.item(i);
+
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+                if ("section".equals(node.getNodeName())) {
+                    // Ignore sections and their content
+                    i += countDescendantNodes(node);
+                }
+            } else if (node.getNodeType() == Node.TEXT_NODE) {
+                contentBuilder.append(node.getTextContent());
+            }
+        }
+
+        return contentBuilder.toString();
+    }
+
+    private static int countDescendantNodes(Node node) {
+        int count = 0;
+        NodeList descendants = node.getChildNodes();
+
+        for (int i = 0; i < descendants.getLength(); i++) {
+            Node descendant = descendants.item(i);
+
+            if (descendant.getNodeType() == Node.ELEMENT_NODE) {
+                count += countDescendantNodes(descendant) + 1;
+            } else {
+                count++;
+            }
+        }
+
+        return count;
     }
 }
