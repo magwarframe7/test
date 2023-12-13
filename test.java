f1604b8613f8e3f054287d1499aa8163a3557904
shivakumar <shivakumar.drive1@gmail.com>
2023-12-14 00:09:13 +0530

    gffghj

--- test.java
+++ test.java
@@ -1,74 +1,60 @@
-import org.w3c.dom.*;
-import org.xml.sax.InputSource;
-
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.DocumentBuilder;
-import java.io.StringReader;
+import java.nio.file.*;
+import java.io.IOException;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
+import java.util.Comparator;
 
 public class test {
+
     public static void main(String[] args) {
-        String xmlString = "<root><title>Main Title</title><content>Some content<subsection>Subsection</subsection>More content</content></root>";
+        String directoryPath = "/path/to/your/directory";
 
         try {
-            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            InputSource inputSource = new InputSource(new StringReader(xmlString));
-            Document document = builder.parse(inputSource);
-
-            // Extract text content
-            String title = extractTitle(document);
-            String content = extractContent(document);
-
-            // Print the results
-            System.out.println("Title: " + title);
-            System.out.println("Content: " + content);
-        } catch (Exception e) {
+            Path latestSubdirectory = getLatestSubdirectory(directoryPath);
+            System.out.println("Latest subdirectory: " + latestSubdirectory);
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static String extractTitle(Document document) {
-        NodeList titleNodes = document.getElementsByTagName("title");
-        if (titleNodes.getLength() > 0) {
-            return titleNodes.item(0).getTextContent();
+    private static Path getLatestSubdirectory(String directoryPath) throws IOException {
+        Path directory = Paths.get(directoryPath);
+
+        // List all subdirectories
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(directory, Files::isDirectory)) {
+            return stream
+                    .map(subdirectory -> new Pair(subdirectory, getLastModifiedTime(subdirectory)))
+                    .max(Comparator.comparing(Pair::getModifiedTime))
+                    .map(Pair::getPath)
+                    .orElse(null);
         }
-        return null;
     }
 
-    private static String extractContent(Document document) {
-        StringBuilder contentBuilder = new StringBuilder();
-        NodeList nodes = document.getDocumentElement().getChildNodes();
-
-        for (int i = 0; i < nodes.getLength(); i++) {
-            Node node = nodes.item(i);
-
-            if (node.getNodeType() == Node.ELEMENT_NODE) {
-                if ("section".equals(node.getNodeName())) {
-                    // Ignore sections and their content
-                    i += countDescendantNodes(node);
-                }
-            } else if (node.getNodeType() == Node.TEXT_NODE) {
-                contentBuilder.append(node.getTextContent());
-            }
+    private static FileTime getLastModifiedTime(Path path) {
+        try {
+            BasicFileAttributes attrs = Files.readAttributes(path, BasicFileAttributes.class);
+            return attrs.lastModifiedTime();
+        } catch (IOException e) {
+            throw new RuntimeException("Error getting last modified time", e);
         }
-
-        return contentBuilder.toString();
     }
 
-    private static int countDescendantNodes(Node node) {
-        int count = 0;
-        NodeList descendants = node.getChildNodes();
+    // Helper class to hold a pair of Path and FileTime
+    private static class Pair {
+        private final Path path;
+        private final FileTime modifiedTime;
 
-        for (int i = 0; i < descendants.getLength(); i++) {
-            Node descendant = descendants.item(i);
-
-            if (descendant.getNodeType() == Node.ELEMENT_NODE) {
-                count += countDescendantNodes(descendant) + 1;
-            } else {
-                count++;
-            }
+        public Pair(Path path, FileTime modifiedTime) {
+            this.path = path;
+            this.modifiedTime = modifiedTime;
         }
 
-        return count;
+        public Path getPath() {
+            return path;
+        }
+
+        public FileTime getModifiedTime() {
+            return modifiedTime;
+        }
     }
 }
